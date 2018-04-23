00a89c6bcc9d2a87fdf6dbd0aa628eda63c396a0
Emily Parkes <emilycoco@me.com>
2018-04-23 16:38:26 +1200

    update tests

--- server/db/db.js
+++ server/db/db.js
@@ -5,6 +5,8 @@ const connection = require('knex')(config)
 module.exports = {
   getPublicEvents,
   getLocalEvents,
+  getPublicEventById,
+  getLocalEventById,
   getPublicEventByName,
   getLocalEventByName,
   getPublicEventsByCategoryId,
@@ -23,12 +25,27 @@ function getLocalEvents(testConn) {
     .select()
 }
 
+function getPublicEventById(id, testConn) {
+  const conn = testConn || connection
+  return conn('public_events')
+    .where('public_events.id', id)
+    .select()
+}
+
+function getLocalEventById(id, testConn) {
+  const conn = testConn || connection
+  return conn('local_events')
+    .where('local_events.id', id)
+    .select()
+}
+
 function getPublicEventByName(eventName, testConn) {
   const conn = testConn || connection
   return conn('public_events')
     .where('public_events.eventName', eventName)
     .select()
 }
+
 function getLocalEventByName(eventName, testConn) {
   const conn = testConn || connection
   return conn('local_events')
--- test/server/db/db.test.js
+++ test/server/db/db.test.js
@@ -10,70 +10,55 @@ beforeEach(() => {
   return env.initialise(testDb)
 })
 
-// afterEach(() => env.cleanup(testDb))
+afterEach(() => env.cleanup(testDb))
 
 test('test environment is operating correctly', () => {
   expect(true).toBeTruthy()
 })
-// //
-// const env = require('./test-environment')
-// const db = require('../db')
 
-// let testDb = null
-
-// beforeEach(() => {
-//   testDb = env.getTestDb()
-//   return env.initialise(testDb)
-// })
-
-// afterEach(() => {
-//   env.cleanup(testDb)
-// })
-
-// test('test environment is operating correctly', () => {
-//   expect(true).toBeTruthy()
-// })
-// //
-
-// test('getDriverVehicles returns 5 drivers', () => {
-//   const expected = 5
-//   return db.getDriverVehicles(testDb)
-//     .then(drivers => {
-//       expect(drivers.length).toBe(expected)
-//     })
-// })
-
-test('getPublicEvents returns 3 events', () => {
-  const expected = 3
+test('getPublicEvents returns 4 events', () => {
+  const expected = 4
   return db.getPublicEvents(testDb)
     .then(events => {
       expect(events.length).toBe(expected)
     })
 })
 
-// test('getLocalEvents returns 3 events', () => {
-//   const expected = 3
-//   return db.getLocalEvents(testDb)
-//     .then(events => {
-//       expect(events.length).toBe(expected)
-//     })
-// })
-
-test('getPublicEventByName returns the event name "rowValue1"', () => {
-  const expected = 'rowValue1'
-  return db.getPublicEventByName(testDb)
-    .then(event => {
-      expect(event).toBe(expected)
+test('getLocalEvents returns 4 events', () => {
+  const expected = 4
+  return db.getLocalEvents(testDb)
+    .then(events => {
+      expect(events.length).toBe(expected)
     })
 })
 
-// test('getLocalEventByName returns the event name', () => {
-//   const expected = ''
-//   return db.getLocalEventByName(testDb)
-//     .then(event => {
-//       expect(event).toBe(expected)
-//     })
-// })
+test('getPublicEventById returns the event id 1', () => {
+  return db.getPublicEventById(1, testDb)
+    .then(events => {
+      expect(events.id).toBe(1)
+    })
+})
+
+test('getLocalEventById returns the event id 2', () => {
+  return db.getLocalEventById(2, testDb)
+    .then(event => {
+      expect(event.id).toBe(2)
+    })
+})
+
+test('getPublicEventByName returns the event name', () => {
+  return db.getPublicEventByName('Farmers Market', testDb)
+    .then(event => {
+      expect(event.eventName).toBe('Farmers Market')
+    })
+})
+
+test('getLocalEventByName returns the event name', () => {
+  return db.getLocalEventByName('Jewellery Sale', testDb)
+    .then(event => {
+      expect(event.eventName).toBe('Jewellery Sale')
+    })
+})
 
 test('getPublicEventsByCategoryId returns the event id 1', () => {
   const expected = 1
@@ -83,14 +68,11 @@ test('getPublicEventsByCategoryId returns the event id 1', () => {
     })
 })
 
-// test('getLocalEventsByCategoryId returns the event id 2', () => {
-//   const expected = 2
-//   return db.getLocalEventsByCategoryId(testDb)
-//     .then(eventId => {
-//       expect(eventId).toBe(expected)
-//     })
-// })
-  
-  
- 
+test('getLocalEventsByCategoryId returns the event id 2', () => {
+  const expected = 2
+  return db.getLocalEventsByCategoryId(testDb)
+    .then(eventId => {
+      expect(eventId).toBe(expected)
+    })
+})
   
