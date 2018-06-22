d7b304df338628580241232c7034e073644a78d2
emily <emilycoco@me.com>
2018-06-22 14:13:23 +1200

    move out of folder, into components

---
+++ client/components/Profile.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+// import { Link } from 'react-router-dom'
+
+// import Preferences from './profile/Preferences'
+
+const Profile = () => {
+  return (
+    <div>
+      <h1>Profile</h1>
+      {/* <Preferences />
+      <button>
+        <Link to='profile/edit' />
+      </button> */}
+    </div>
+  )
+}
+
+export default Profile
+
