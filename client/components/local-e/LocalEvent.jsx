d9f493323c1d3d36b0019776f6843ce78e3b6264
emily <emilycoco@me.com>
2018-06-26 15:00:18 +1200

    change naming

--- client/components/local-e/LocalEvent.jsx
+++ client/components/local-e/LocalEvent.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+// import { Link } from 'react-router-dom'
 import { connect } from 'react-redux'
 
 import { getLocalEventByName } from '../../actions/l-event'
 
 class LocalEvent extends React.Component {
+  constructor(props) {
+  super(props)
+  }
  
   componentDidMount() {
     this.props.dispatch(getLocalEventByName(this.props.match.params.eventName))
@@ -17,11 +20,11 @@ class LocalEvent extends React.Component {
         <div className='page-heading-section'>
           <div className='page-title-font'>
             <h1>Local Event...</h1>
-            <h1>{this.props.event.eventName}</h1></div>
+            <h1>{event.eventName}</h1></div>
         </div>
 
         <div className='page-section'>
-          {event.date}
+          <h1>{event.date}</h1>
           {event.time}
           {event.location}
           {event.address}
