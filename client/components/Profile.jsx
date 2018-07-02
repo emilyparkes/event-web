dd0361fe2cad5eadcd1c80169ccea8238449f5de
emily <emilycoco@me.com>
2018-07-03 11:36:20 +1200

    update and match edit

--- client/components/Profile.jsx
+++ client/components/Profile.jsx
@@ -6,32 +6,43 @@ import { Link } from 'react-router-dom'
 const Profile = () => {
   return (
     <div className='profilepg'>
-        <div className='page-heading-section'>
-          <div className='page-title-font'>
-          <h1>Profile</h1>
+      <div className='page-heading-section'>
+        
+        <div className='profile-pic'>
+          <img src="" alt="" />
         </div>
+        
+        <div className='display-name'>
+          <h1>Emily Parkes</h1>
+        </div>
+        
+        <div className='location'>
+        <h5>Auckland</h5>
+        </div>
+
       </div>
+
       <div className='settings' >
         {/* <Preferences /> */}
-      <button>
-        <Link to='/profile/edit'>edit</Link>
-      </button>
+        <button>
+          <Link to='/profile/edit'>edit</Link>
+        </button>
       </div>
-      
-        <div className='notify'>
-          <div className='invites'>
-            {/* <img className='icon-invite' src='/icons/icon-invite.png' /> */}
-          </div>
-          <div className='friendslist'>
-            {/* <img className='icon-invite' src='/icons/icon-invite.png' /> */}
-          </div>
-          <div className='interested-in'>
-            {/* <img className='icon-invite' src='/icons/icon-invite.png' /> */}
-            </div>
-          <div className='going-to'>
-            {/* <img className='icon-invite' src='/icons/icon-invite.png' /> */}
-            </div>
+
+      <div className='notify'>
+        <div className='invites'>
+          {/* <img className='icon-invite' src='/icons/icon-invite.png' /> */}
         </div>
+        <div className='friends-list'>
+          {/* <img className='icon-invite' src='/icons/icon-invite.png' /> */}
+        </div>
+        <div className='interested-in'>
+          {/* <img className='icon-invite' src='/icons/icon-invite.png' /> */}
+        </div>
+        <div className='going-to'>
+          {/* <img className='icon-invite' src='/icons/icon-invite.png' /> */}
+        </div>
+      </div>
 
     </div>
   )
