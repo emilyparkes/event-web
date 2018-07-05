e83e065fbbdf56c915ad58f821ab74cbbb9ed145
emily <emilycoco@me.com>
2018-07-06 10:45:17 +1200

    /auth on baseurl

--- client/lib/apiClient.js
+++ client/lib/apiClient.js
@@ -3,7 +3,7 @@ import request from 'superagent'
 import {getToken} from './token'
 import {isAuthenticated} from './auth'
 
-const baseUrl = '/api/v1'
+const baseUrl = '/api/v1/auth'
 
 export default function consume (method = 'get', endpoint, data = {}) {
   const dataMethod = method.toLowerCase() === 'get' ? 'query' : 'send'
@@ -11,11 +11,9 @@ export default function consume (method = 'get', endpoint, data = {}) {
   const headers = {
     Accept: 'application/json'
   }
-
   if (isAuthenticated()) {
     headers['Authorization'] = `Bearer ${token}`
   }
-
   return request[method](baseUrl + endpoint)
     .set(headers)[dataMethod](data)
     .then((res) => {
