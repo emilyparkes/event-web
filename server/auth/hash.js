27c9bd00d6fd15af4ef63bff7a4a64b282614e71
emily <emilycoco@me.com>
2018-07-06 10:54:02 +1200

    rename function and remove commented code

--- server/auth/hash.js
+++ server/auth/hash.js
@@ -1,11 +1,11 @@
 const sodium = require('sodium').api
 
 module.exports = {
-  getHash,
+  generate,
   verifyUser
 }
 
-function getHash (password) {
+function generate (password) {
   const passwordBuffer = Buffer.from(password, 'utf8')
   return sodium.crypto_pwhash_str(
     passwordBuffer,
@@ -18,24 +18,3 @@ function verifyUser (hash, password) {
     const passwordBuffer = Buffer.from(password, 'utf8')
     return sodium.crypto_pwhash_str_verify(hash, passwordBuffer)
   }
-
-// const sodium = require('sodium').api
-
-// module.exports = {
-//   getHash,
-//   verifyUser
-// }
-
-// function getHash (password) {
-//   const passwordBuffer = Buffer.from(password, 'utf8')
-//   return sodium.crypto_pwhash_str(
-//     passwordBuffer,
-//     sodium.crypto_pwhash_OPSLIMIT_INTERACTIVE,
-//     sodium.crypto_pwhash_MEMLIMIT_INTERACTIVE
-//   )
-// }
-
-// function verifyUser (hash, password) {
-//   const passwordBuffer = Buffer.from(password, 'utf8')
-//   return sodium.crypto_pwhash_str_verify(hash, passwordBuffer)
-// }
