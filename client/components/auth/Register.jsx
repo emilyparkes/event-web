17e3d59877945d4a4ba94048663f24f6d8446ba9
emily <emilycoco@me.com>
2018-07-09 15:27:28 +1200

    update register form

--- client/components/auth/Register.jsx
+++ client/components/auth/Register.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { connect } from 'react-redux'
 
 import { registerUser, registerError } from '../../actions/auth/register'
-// import ErrorMessage from './ErrorMessage'
+import ErrorMessage from './ErrorMessage'
 
 class Register extends React.Component {
   constructor(props) {
@@ -43,26 +43,34 @@ class Register extends React.Component {
   render() {
     const { displayname, email, username, password, confirm } = this.state
     return (
+      <div className='registerpg'>
+      <div className='register-title'>
+      <h2>Register with Eventer</h2></div>
+
+      <div className='error-msg'>
+      <ErrorMessage />
+      </div>
+
       <form>
-        <p><input name='displayname' placeholder='displayname'
+        <p><input name='displayname' placeholder='displayname' autoComplete='name'
           onChange={this.handleChange} value={displayname} /></p>
 
-        <p><input name='email' placeholder='email'
+        <p><input name='email' placeholder='email' autoComplete='email'
           onChange={this.handleChange} value={email} /></p>
 
-        <p><input name='username' placeholder='Username'
+        <p><input name='username' placeholder='Username' autoComplete='username'
           onChange={this.handleChange} value={username} /></p>
 
-        <p><input type='password' name='password' placeholder='Password'
+        <p><input type='password' name='password' placeholder='Password' autoComplete='new-password'
           onChange={this.handleChange} value={password} /></p>
 
-        <p><input type='password' name='confirm' placeholder='Confirm'
+        <p><input type='password' name='confirm' placeholder='Confirm' autoComplete='new-password'
           onChange={this.handleChange} value={confirm} /></p>
 
         <button onClick={this.handleClick}>Register</button>
 
-        {/* <ErrorMessage reducer='auth' /> */}
       </form>
+      </div>
     )
   }
 }
