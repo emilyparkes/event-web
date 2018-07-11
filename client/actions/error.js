16bf9017e061c1afe0c3708a1207d314a9d52a49
emily <emilycoco@me.com>
2018-07-11 12:15:30 +1200

    add sucess alert

--- client/actions/error.js
+++ client/actions/error.js
@@ -1,14 +1,22 @@
+export const SHOW_SUCCESS = 'SHOW_SUCCESS'
 export const SHOW_ERROR = 'SHOW_ERROR'
 export const CLEAR_ERROR = 'CLEAR_ERROR'
 
-export function showError (errorMessage) {
+export function showSuccess (message) {
   return {
-    type: SHOW_ERROR,
-    errorMessage
+    type: SHOW_SUCCESS,
+    message
   }
 }
 
-export function clearError (errorMessage) {
+export function showError (message) {
+  return {
+    type: SHOW_ERROR,
+    message
+  }
+}
+
+export function clearError () {
   return {
     type: CLEAR_ERROR
   }
