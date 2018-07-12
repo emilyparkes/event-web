a489b84ca7cfa93df7d88983b1bf5e8c9234897b
emily <emilycoco@me.com>
2018-07-12 16:47:24 +1200

    get profileinfo fns

---
+++ server/db/user-profiles.js
@@ -0,0 +1,32 @@
+const environment = process.env.NODE_ENV || 'development'
+const config = require('./knexfile')[environment]
+const connection = require('knex')(config)
+
+module.exports = {
+  getUserProfile,
+  updateUserProfile
+}
+
+function getUserProfile(userId, conn) {
+  const db = conn || connection
+  return db('user_profiles')
+    .select()
+    .where('user_id', userId)
+    .first()
+
+}
+
+function updateUserProfile(userId, conn) {
+  const db = conn || connection
+  return db('user_profiles')
+    .select('firstName', 'preferredName', 'surname', 'profilePic', 'phone', 'bio')
+    .where('user_id', userId)
+    .update({
+      firstName,
+      preferredName,
+      surname,
+      profilePic,
+      phone,
+      bio
+    })
+}
