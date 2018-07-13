2814da8eabd6f1e0a9e044a47233eadc3e9c8134
emily <emilycoco@me.com>
2018-07-13 16:35:01 +1200

    create database profile fns

--- server/db/user-profiles.js
+++ server/db/user-profiles.js
@@ -11,22 +11,23 @@ function getUserProfile(userId, conn) {
   const db = conn || connection
   return db('user_profiles')
     .select()
+    // 'firstName', 'preferredName', 'surname', 'profilePic', 'bio', 'phone', 'location'
     .where('user_id', userId)
     .first()
-
 }
 
 function updateUserProfile(userId, conn) {
   const db = conn || connection
   return db('user_profiles')
-    .select('firstName', 'preferredName', 'surname', 'profilePic', 'phone', 'bio')
+    .select()
     .where('user_id', userId)
     .update({
       firstName,
       preferredName,
       surname,
       profilePic,
+      bio,
       phone,
-      bio
+      location
     })
 }
