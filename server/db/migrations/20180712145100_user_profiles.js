cfd05a4c17333076b8c52a72747bfe1c40ab4d34
emily <emilycoco@me.com>
2018-07-16 11:18:57 +1200

    update migrations and db fns

---
+++ server/db/migrations/20180712145100_user_profiles.js
@@ -0,0 +1,21 @@
+exports.up = (knex, Promise) => {
+  return knex.schema.hasTable('user_profiles').then(function (exists) {
+    if (!exists) {
+      return knex.schema.createTable('user_profiles', (table) => {
+        table.increments('id').primary()
+        table.integer('user_id').references('users.id')
+        table.string('firstName')
+        table.string('preferredName')
+        table.string('surname')
+        table.string('profilePic')
+        table.string('bio')
+        table.string('phone')
+        table.string('location')
+      })
+    }
+  })
+}
+
+exports.down = (knex, Promise) => {
+  return knex.schema.dropTableIfExists('user_profiles')
+}
