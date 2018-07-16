eaf99ea2c94658201799cb085c45bc0df97469fb
emily <emilycoco@me.com>
2018-07-16 14:45:49 +1200

    update seeds for new structure

--- test/server/db/seeds/03_events_categories_junction.js
+++ test/server/db/seeds/03_events_categories_junction.js
@@ -11,7 +11,15 @@ exports.seed = function(knex, Promise) {
         {id: 4, event_id: 3, category_id: 2},
         {id: 5, event_id: 4, category_id: 2},
         {id: 6, event_id: 4, category_id: 3},
-        {id: 7, event_id: 4, category_id: 5}
+        {id: 7, event_id: 4, category_id: 5},
+        {id: 8, event_id: 5, category_id: 7},
+        {id: 9, event_id: 5, category_id: 9},
+        {id: 10, event_id: 6, category_id: 9},
+        {id: 11, event_id: 6, category_id: 10},
+        {id: 12, event_id: 7, category_id: 6},
+        {id: 13, event_id: 7, category_id: 7},
+        {id: 14, event_id: 8, category_id: 10},
+        {id: 15, event_id: 8, category_id: 8}
       ])
     })
 }
