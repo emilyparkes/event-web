da7c6a44e9cf5675e71b03e7e014fa61a8095432
emily <emilycoco@me.com>
2018-07-16 14:53:55 +1200

    update seed data

--- test/server/db/seeds/06_user_interestedin_events.js
+++ test/server/db/seeds/06_user_interestedin_events.js
@@ -6,8 +6,7 @@ exports.seed = function(knex, Promise) {
       // Inserts seed entries
       return knex('user_interestedin_events').insert([
         {id: 1, user_id: 1, event_id: 2},
-        {id: 2, user_id: 1, event_id: 2}
-       
+        {id: 2, user_id: 1, event_id: 5}
       ])
     })
 }
