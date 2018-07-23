c5eafcb2e012b3637506ff050510a28f6549a90e
emily <emilycoco@me.com>
2018-07-23 16:50:40 +1200

    public-e  action tests

---
+++ test/client/actions/p-event.test.js
@@ -0,0 +1,33 @@
+/* global test expect */
+
+import { receivePublicEventByName } from '../../../client/actions/public-e/p-event'
+
+test('receivePublicEventByName returns the correct action type', () => {
+  const expected = 'RECEIVE_PUBLIC_EVENT_BY_NAME'
+
+  const actual = receivePublicEventByName()
+
+  expect(actual.type).toBe(expected)
+})
+
+test('receivePublicEventByName returns the correct action', () => {
+  const expected = {
+    type: 'RECEIVE_PUBLIC_EVENT_BY_NAME'
+  }
+
+  const actual = receivePublicEventByName()
+
+  expect(actual).toEqual(expected)
+})
+
+test('receivePublicEventByName returns the event', () => {
+  const pEvent = {id: 2, eventName: 'Moon-Festival', imageUrl: 'http://lorempixel.com/400/200', date: 'Saturday 26 May', location: 'CBD', address: 'Queen St', time: '7pm-2am', ticketInfo: 'Free', ticketUrl: '', website: 'www.moonfestival.akl', blurb: 'A festival to celebrate the night and full moon.' ,  eventType: 1}
+  const expected = {
+    type: 'RECEIVE_PUBLIC_EVENT_BY_NAME',
+    pEvent
+  }
+
+  const actual = receivePublicEventByName(pEvent)
+
+  expect(actual).toEqual(expected)
+})
