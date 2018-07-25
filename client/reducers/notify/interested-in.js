58cbf1218178f710ad8f6c78e52ed1e5d73d80ff
emily <emilycoco@me.com>
2018-07-25 16:16:26 +1200

    interested in complete

---
+++ client/reducers/notify/interested-in.js
@@ -0,0 +1,15 @@
+import {
+  RECEIVE_INTERESTED_IN_LIST } from '../../actions/notify/interested-in'
+
+const initialState = []
+
+const interestedIn = (state = initialState, action) => {
+  switch (action.type) {
+    case RECEIVE_INTERESTED_IN_LIST:
+      return action.interestedIn
+    default:
+      return state
+  }
+}
+
+export default interestedIn
