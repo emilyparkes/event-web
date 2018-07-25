3274432033af8e0632b5326e1f9cbe5c40a73459
emily <emilycoco@me.com>
2018-07-25 16:13:51 +1200

    interested in actions and new api routes

---
+++ client/actions/notify/interested-in.js
@@ -0,0 +1,26 @@
+import request from 'superagent'
+
+import { showError } from '../error'
+import baseUrl from '../../lib/base-url'
+
+export const RECEIVE_INTERESTED_IN_LIST = 'RECEIVE_INTERESTED_IN_LIST'
+
+export const receiveInterestedInList = (interestedIn) => {
+  return {
+    type: RECEIVE_INTERESTED_IN_LIST,
+    interestedIn
+  }
+}
+
+export function getInterestedInList() {
+  return (dispatch) => {
+    request.get(`${baseUrl}/api/v1/notify/interests`)
+      .then(res => {
+        console.log(res.body.interestedIn)
+        dispatch(receiveInterestedInList(res.body.interestedIn))
+      })
+      .catch((err) => {
+        dispatch(showError(err.message))
+      })
+  }
+}
