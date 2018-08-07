93528158d5fe5864ee495148fbb771045e69c5cd
emily <emilycoco@me.com>
2018-08-07 12:40:53 +1200

    alter table names for invitedto fn

--- server/db/notify.js
+++ server/db/notify.js
@@ -4,7 +4,9 @@ const connection = require('knex')(config)
 
 module.exports = {
   getFriendsList,
-  getInterestedInList
+  getInterestedInList,
+  getGoingToList,
+  getInvitedToList
 }
 
 
@@ -22,5 +24,24 @@ function getInterestedInList(id, conn) {
     .join('users', 'users.id', 'user_id')
     .join('events', 'events.id', 'event_id')
     .where('user_id', id)
+    .select()
+}
+
+function getGoingToList(id, conn) {
+  const db = conn || connection
+  return db('user_goingto_events')
+    .join('users', 'users.id', 'user_id')
+    .join('events', 'events.id', 'event_id')
+    .where('user_id', id)
+    .select()
+}
+
+function getInvitedToList(id, conn) {
+  const db = conn || connection
+  return db('user_invitedto_events')
+    .join('users', 'users.id', 'invitee_user_id')
+    .join('users', 'users.id', 'inviter_user_id')
+    .join('events', 'events.id', 'event_id')
+    .where('users.id', id)
     .select()
 }
