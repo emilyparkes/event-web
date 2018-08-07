7a05f1db015ba39c9569d18820de8f02eb4edd95
emily <emilycoco@me.com>
2018-08-07 14:22:18 +1200

    invited to reducer

---
+++ client/reducers/notify/invited-to.js
@@ -0,0 +1,15 @@
+import {
+  RECEIVE_INVITED_TO_LIST } from '../../actions/notify/invited-to'
+
+const initialState = []
+
+const invitedTo = (state = initialState, action) => {
+  switch (action.type) {
+    case RECEIVE_INVITED_TO_LIST:
+      return action.invitedTo
+    default:
+      return state
+  }
+}
+
+export default invitedTo
