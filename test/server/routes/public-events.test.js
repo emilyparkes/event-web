e7f3ed3d2af61f7d34fc43d236e7fc2e18f5f958
emily <emilycoco@me.com>
2018-08-13 11:16:11 +1200

    add test environment test to all test files

---
+++ test/server/routes/public-events.test.js
@@ -0,0 +1,6 @@
+const request = require('supertest')
+const server = require('../../../server/server')
+
+test('test environment for public-events routes is operating correctly', () => {
+  expect(true).toBeTruthy()
+})
---
+++ test/server/routes/users.test.js
@@ -0,0 +1,6 @@
+const request = require('supertest')
+const server = require('../../../server/server')
+
+test('test environment for users routes is operating correctly', () => {
+  expect(true).toBeTruthy()
+})
