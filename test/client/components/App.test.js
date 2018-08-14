d8f71983821023b42974392a14dc8a53cefd55b9
emily <emilycoco@me.com>
2018-08-14 14:34:37 +1200

    App test

--- test/client/components/App.test.js
+++ test/client/components/App.test.js
@@ -1,14 +1,19 @@
 import React from 'react'
-import Enzyme, {shallow, render, mount} from 'enzyme'
-import Adapter from 'enzyme-adapter-react-16'
-import './setup-env'
+import { shallow } from 'enzyme'
 
 import App from '../../../client/components/App'
 
-App.prototype.componentDidMount = () => {}
-
-Enzyme.configure({adapter: new Adapter()})
-
 test('test environment for app component is operating correctly', () => {
   expect(true).toBeTruthy()
+})
+
+test('<App /> contains a header, switch and footer', () => {
+  const wrapper = shallow(<App />)
+  expect(wrapper.text()).toBe('<Header /><Switch /><Footer />')
+})
+
+test('<App> root has className of app', () => {
+  const wrapper = shallow(<App />)
+  const root = wrapper.find('.app')
+  expect(root.length).toBe(1)
 })
