49845cabb906e18865698e697c3739289228b850
emily <emilycoco@me.com>
2018-08-16 15:57:23 +1200

    eventfinder results (tests) page created

---
+++ client/components/Eventfinda.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+class Eventfinda extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return (
+      <div className='Eventfindapg'>
+        <div className='page-heading-section'>
+          <div className='page-title-font'>
+            <h1>Eventfinda</h1>
+          </div>
+          <h6>Do you want to eventify your life? To make it fuller and brighter? Find your ideal events here.</h6>
+        </div>
+
+
+        <div className='page-section'>
+        {this.props.name}
+        {this.props.url}
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Eventfinda
