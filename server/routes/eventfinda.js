7071d34719baa8901bf952e47f0013c00cc4685d
emily <emilycoco@me.com>
2018-08-17 16:31:57 +1200

    correct send

--- server/routes/eventfinda.js
+++ server/routes/eventfinda.js
@@ -6,21 +6,23 @@ const request = require('superagent')
 router.use(express.json())
 router.use(express.static(path.join(__dirname, './public')))
 
+const buf = Buffer.from('eventweb' + ':' + 'rh526df55bvm')
+const headerValue = `Basic ${buf.toString('base64')}`
+
+
 
 router.get('/eventf', (req, res) => {
-  const buf = Buffer.from('eventweb' + ':' + 'rh526df55bvm')
-  const headerValue = `Basic ${buf.toString('base64')}`
-
   request.get('http://api.eventfinda.co.nz/v2/events.json')
     .set("Authorization", headerValue)
-    .then(res => {
-      const events = res.body.events[0]
-      console.log(res.body.events[0])
-      res.json(events)
+    .then(resp => {
+      const events = resp.body.events[0]
+      console.log(resp.body.events[0])
+      res.send(events)
     })
     .catch(err => {
-      res.send({ error: err.message })
+      res.status(500).send(err.message)
     })
 })
 
 module.exports = router
+
