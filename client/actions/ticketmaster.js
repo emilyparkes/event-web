34e124ceee5a0fe19b5b070c4827a9c403f1abeb
emily <emilycoco@me.com>
2018-08-21 14:57:46 +1200

    remove extention on res.body, rename to getApi

---
+++ client/actions/ticketmaster.js
@@ -0,0 +1,25 @@
+import request from 'superagent'
+
+import { showError } from './error'
+
+export const RECEIVE_API = 'RECEIVE_API'
+
+export const receiveApi = (events) => {
+  return {
+    type: RECEIVE_API,
+    events
+  }
+}
+
+export function getApi() {
+  return (dispatch) => {
+    request.get('/api/v1/ticketmaster/events')
+      .then(res => {
+        dispatch(receiveApi(res.body))
+        console.log(res.body)
+      })
+      .catch((err) => {
+        dispatch(showError(err.message))
+      })
+  }
+}   
