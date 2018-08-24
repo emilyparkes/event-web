156fe8f43a44aa90050498c707a6852373c9a322
emily <emilycoco@me.com>
2018-08-24 16:27:25 +1200

    add seed no

--- test/server/db/seeds/11_events_venues_junction.js
+++ test/server/db/seeds/11_events_venues_junction.js
@@ -1,10 +1,10 @@
 
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('events_venues_junction').del()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
+      return knex('events_venues_junction').insert([
         {id: 1, venue_id: 1, event_id: 1},
         {id: 2, venue_id: 1, event_id: 2},
         {id: 3, venue_id: 1, event_id: 3},
