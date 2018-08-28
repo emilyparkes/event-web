fc4c65b04cbc8911179593f705bd09957a2a60bf
emily <emilycoco@me.com>
2018-08-28 17:14:10 +1200

    added req instead of res

--- server/routes/add.js
+++ server/routes/add.js
@@ -8,13 +8,27 @@ module.exports = router
 
 router.use(bodyParser.json())
 
-router.post('/create-event', (req, res) => {
-  newEvent = res.body
-  db.createEvent(newEvent)
-    // .then((event) => {
-    //   res.send(event)
-    // })
+router.get('/events', (req, res) => {
+  request.get('https://geocoder.api.here.com/6.2/geocode.json?app_id={hE5OawYW1TBeyN5UI9vc}&app_code={E_40_tJ5ujOwuSeLFLO8xw}')
+    .then(resp => {
+      const data = resp.body
+      console.log(resp.body)
+      res.send(data)
+    })
     .catch(err => {
       res.status(500).send(err.message)
     })
 })
+
+
+
+router.post('/create-event', (req, res, next) => {
+  newEvent = req.body
+  db.createEvent(newEvent)
+    .then(() => next()) 
+    .catch(err => {
+      res.status(500).send(err.message)
+    })
+})
+
+
