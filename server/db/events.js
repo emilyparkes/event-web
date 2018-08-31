b75f47abbabfadad24e1e8d914090ce36c132ff8
emily <emilycoco@me.com>
2018-08-31 12:24:02 +1200

    use venues to test lat lng fns

--- server/db/events.js
+++ server/db/events.js
@@ -115,37 +115,45 @@ function getEventFromCategory(category, eventName, conn) {
 
 function createEvent(newEvent, conn) {
   const db = conn || connection
-  return db('events')
+  return db('venues')
     .insert({
-      eventName: newEvent.eventName,
-      date_start: newEvent.dateStart,
-      time_start: newEvent.timeStart,
-      date_end: newEvent.dateEnd,
-      time_end: newEvent.timeEnd,
-      image: newEvent.image,
-      description: newEvent.description,
-      access: newEvent.access,
-      tickets: newEvent.tickets,
-      restrictions: newEvent.restrictions
-    // })
-    // .then((eId) => {
-    //   return db('events_venues_junction')
-    //     .insert({
-    //       event_id: eId[0],
-    //     })
-    //     .then((vId) => {
-    //       return db('events_venues_junction')
-    //         .insert({
-    //           id: vId[0],
-    //           venueName: newEvent.venueName,
-    //           venueAddress: newEvent.venueAddress,
-    //           townSuburb: newEvent.townSuburb,
-    //           region: newEvent.region
-    //         })
-    //     })
+      venueName: newEvent.venueName,
+      address: newEvent.address,
+      suburb: newEvent.suburb,
+      region: newEvent.region,
+      postal: newEvent.postal,
+      country: 'New Zealand',
+      lat: newEvent.lat,
+      lng: newEvent.lng
     })
+  // .then((eId) => {
+  //   return db('events_venues_junction')
+  //     .insert({
+  //       event_id: eId[0],
+  //     })
+  //     .then((vId) => {
+  //       return db('events_venues_junction')
+  //         .insert({
+  //           id: vId[0],
+  //           venueName: newEvent.venueName,
+  //           address: newEvent.address,
+  //           suburb: newEvent.suburb,
+  //           region: newEvent.region
+  //         })
+  // })
+  // })
 }
 
+// eventName: newEvent.eventName,
+// date_start: newEvent.dateStart,
+// time_start: newEvent.timeStart,
+// date_end: newEvent.dateEnd,
+// time_end: newEvent.timeEnd,
+// image: newEvent.image,
+// description: newEvent.description,
+// access: newEvent.access,
+// tickets: newEvent.tickets,
+// restrictions: newEvent.restrictions
 
 
 // return db('users')
