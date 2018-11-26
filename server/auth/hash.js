226db03fe1422f8f4bbff7c619e87010687afc88
emily <emilycoco@me.com>
2018-11-26 14:24:52 +1300

    new version of hash

--- server/auth/hash.js
+++ server/auth/hash.js
@@ -1,20 +1,20 @@
-// const sodium = require('sodium').api
+const sodium = require('libsodium-wrappers')
 
-// module.exports = {
-//   generate,
-//   verify
-// }
+module.exports = {
+  generateHash,
+  verify
+}
 
-// function generate (password) {
-//   const passwordBuffer = Buffer.from(password, 'utf8')
-//   return sodium.crypto_pwhash_str(
-//     passwordBuffer,
-//     sodium.crypto_pwhash_OPSLIMIT_INTERACTIVE,
-//     sodium.crypto_pwhash_MEMLIMIT_INTERACTIVE
-//   )
-// }
-
-// function verify (hash, password) {
-//   const passwordBuffer = Buffer.from(password, 'utf8')
-//   return sodium.crypto_pwhash_str_verify(hash, passwordBuffer)
-// }
+function generateHash (password) {
+  return sodium.ready.then(() =>
+    sodium.crypto_pwhash_str(
+      password,
+      sodium.crypto_pwhash_OPSLIMIT_INTERACTIVE,
+      sodium.crypto_pwhash_MEMLIMIT_MIN
+    )
+  )
+}
+function verify (hash, password) {
+  const passwordBuffer = Buffer.from(password, 'utf8')
+  return sodium.crypto_pwhash_str_verify(hash, passwordBuffer)
+}
