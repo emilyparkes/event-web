2c5b3c66a3312373f488e4c876a2d18332c538f9
emily <emilycoco@me.com>
2018-11-26 14:26:28 +1300

    and unique to username

--- server/db/migrations/20180703144818_users.js
+++ server/db/migrations/20180703144818_users.js
@@ -4,7 +4,7 @@ exports.up = (knex, Promise) => {
       return knex.schema.createTable('users', (table) => {
         table.increments('id').primary()
         table.string('email')
-        table.string('username')
+        table.string('username').unique()
         table.string('hash')
       })
     }
