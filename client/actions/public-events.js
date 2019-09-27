6917e792bf08e7136ad6eaab14351124f7846bbb
Emily Coco Parkes <emilycoco@me.com>
2019-09-27 16:30:58 +1200

    condense public events

--- client/actions/public-events.js
+++ client/actions/public-events.js
@@ -1,9 +1,29 @@
 import request from 'superagent'
 
-import {showError} from '../error'
+import { showError } from '../error'
 import baseUrl from '../../lib/base-url'
 
+export const RECEIVE_PUBLIC_EVENTS = 'RECEIVE_PUBLIC_EVENTS'
 export const RECEIVE_PUBLIC_EVENT_BY_NAME = 'RECEIVE_PUBLIC_EVENT_BY_NAME'
+
+export const receivePublicEvents = (publicEvents) => {
+  return {
+    type: RECEIVE_PUBLIC_EVENTS,
+    publicEvents
+  }
+}
+
+export function getPublicEvents () {
+  return (dispatch) => {
+    request.get(`${baseUrl}/api/v1/public-events`)
+      .then(res => {
+        dispatch(receivePublicEvents(res.body.publicEvents))
+      })
+      .catch(() => {
+        dispatch(showError('An unexpected error in getting the events information'))
+      })
+  }
+}
 
 export const receivePublicEventByName = (publicEvent) => {
   return {
---
+++ client/actions/public-events.test.js
@@ -0,0 +1,64 @@
+/* global test expect */
+
+import { receivePublicEvents } from './public-events'
+import { receivePublicEventByName } from './p-event'
+
+test('receivePublicEvents returns the correct action type', () => {
+  const expected = 'RECEIVE_PUBLIC_EVENTS'
+
+  const actual = receivePublicEvents()
+
+  expect(actual.type).toBe(expected)
+})
+
+test('receivePublicEvents returns the correct action', () => {
+  const expected = {
+    type: 'RECEIVE_PUBLIC_EVENTS'
+  }
+
+  const actual = receivePublicEvents()
+
+  expect(actual).toEqual(expected)
+})
+
+test('receivePublicEvents returns the public events', () => {
+  const publicEvents = ['Farmers-Market', 'Moon-Festival']
+  const expected = {
+    type: 'RECEIVE_PUBLIC_EVENTS',
+    publicEvents: ['Farmers-Market', 'Moon-Festival']
+  }
+
+  const actual = receivePublicEvents(publicEvents)
+
+  expect(actual).toEqual(expected)
+})
+
+test('receivePublicEventByName returns the correct action type', () => {
+  const expected = 'RECEIVE_PUBLIC_EVENT_BY_NAME'
+
+  const actual = receivePublicEventByName()
+
+  expect(actual.type).toBe(expected)
+})
+
+test('receivePublicEventByName returns the correct action', () => {
+  const expected = {
+    type: 'RECEIVE_PUBLIC_EVENT_BY_NAME'
+  }
+
+  const actual = receivePublicEventByName()
+
+  expect(actual).toEqual(expected)
+})
+
+test('receivePublicEventByName returns the event', () => {
+  const publicEvent = { id: 2, eventName: 'Moon-Festival', imageUrl: 'http://lorempixel.com/400/200', date: 'Saturday 26 May', location: 'CBD', address: 'Queen St', time: '7pm-2am', ticketInfo: 'Free', ticketUrl: '', website: 'www.moonfestival.akl', blurb: 'A festival to celebrate the night and full moon.', eventType: 1 }
+  const expected = {
+    type: 'RECEIVE_PUBLIC_EVENT_BY_NAME',
+    publicEvent
+  }
+
+  const actual = receivePublicEventByName(publicEvent)
+
+  expect(actual).toEqual(expected)
+})
