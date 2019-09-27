137ca349e59eb0b6443a3f2d77dc9ec9abd59598
Emily Coco Parkes <emilycoco@me.com>
2019-09-27 16:28:01 +1200

    update

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,39 +1,26 @@
 const path = require('path')
-var webpack = require('webpack')
 
 module.exports = {
-  entry: path.join(__dirname, './index.js'),
+  entry: './client/index.js',
   output: {
-    path: path.join(__dirname, '../public'),
+    path: path.join(__dirname, '../server/public'),
     filename: 'bundle.js'
   },
+  mode: 'development',
   module: {
     rules: [
       {
         test: /\.jsx?$/,
-        exclude: /node_modules/,
-        use: [{
-          loader: 'babel-loader'
-        }]
-      },
-      {
-        test: /\.css$/,
-        use: [
-          {loader: 'style-loader'},
-          {loader: 'css-loader'}
-        ]
+        loader: 'babel-loader',
+        exclude: /node_modules/
       }
     ]
   },
   resolve: {
     extensions: ['.js', '.jsx']
   },
-  plugins: [
-    new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: JSON.stringify(process.env.NODE_ENV)
-      }
-    })
-  ],
-  devtool: 'source-map'
+  devtool: 'source-map',
+  devServer: {
+    contentBase: '../server/public'
+  }
 }
