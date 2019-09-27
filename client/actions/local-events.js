3c161f10607338fac54d1fe70da1da4aa9f0bc4f
Emily Coco Parkes <emilycoco@me.com>
2019-09-27 16:29:20 +1200

    condense local events

--- client/actions/local-events.js
+++ client/actions/local-events.js
@@ -1,7 +1,28 @@
 import request from 'superagent'
 
-import {showError} from '../error'
+import { showError } from '../error'
 import baseUrl from '../../lib/base-url'
+
+export const RECEIVE_LOCAL_EVENTS = 'RECEIVE_LOCAL_EVENTS'
+
+export const receiveLocalEvents = (localEvents) => {
+  return {
+    type: RECEIVE_LOCAL_EVENTS,
+    localEvents
+  }
+}
+
+export function getLocalEvents () {
+  return (dispatch) => {
+    request.get(`${baseUrl}/api/v1/local-events`)
+      .then(res => {
+        dispatch(receiveLocalEvents(res.body.localEvents))
+      })
+      .catch(() => {
+        dispatch(showError('An unexpected error in getting information'))
+      })
+  }
+}
 
 export const RECEIVE_LOCAL_EVENT_BY_NAME = 'RECEIVE_LOCAL_EVENT_BY_NAME'
 
---
+++ client/actions/local-events.test.js
@@ -0,0 +1,64 @@
+/* global test expect */
+
+import { receiveLocalEvents } from './local-events'
+import { receiveLocalEventByName } from './l-event'
+
+test('receiveLocalEvents returns the correct action type', () => {
+  const expected = 'RECEIVE_LOCAL_EVENTS'
+
+  const actual = receiveLocalEvents()
+
+  expect(actual.type).toBe(expected)
+})
+
+test('receiveLocalEvents returns the correct action', () => {
+  const expected = {
+    type: 'RECEIVE_LOCAL_EVENTS'
+  }
+
+  const actual = receiveLocalEvents()
+
+  expect(actual).toEqual(expected)
+})
+
+test('receiveLocalEvents returns the LocalEvents', () => {
+  const localEvents = ['Jewellery-Sale', 'Community-Garden-Day']
+  const expected = {
+    type: 'RECEIVE_LOCAL_EVENTS',
+    localEvents: ['Jewellery-Sale', 'Community-Garden-Day']
+  }
+
+  const actual = receiveLocalEvents(localEvents)
+
+  expect(actual).toEqual(expected)
+})
+
+test('receiveLocalEventByName returns the correct action type', () => {
+  const expected = 'RECEIVE_LOCAL_EVENT_BY_NAME'
+
+  const actual = receiveLocalEventByName()
+
+  expect(actual.type).toBe(expected)
+})
+
+test('receiveLocalEventByName returns the correct action', () => {
+  const expected = {
+    type: 'RECEIVE_LOCAL_EVENT_BY_NAME'
+  }
+
+  const actual = receiveLocalEventByName()
+
+  expect(actual).toEqual(expected)
+})
+
+test('receiveLocalEventByName returns the event', () => {
+  const lEvent = { id: 5, eventName: 'Jewellery-Sale', imageUrl: 'http://lorempixel.com/400/200', date: 'Saturday 25 May', location: 'My House', address: '112 road', time: '10am-3pm', ticketInfo: 'Free', ticketUrl: '', website: 'www.sallysells.shop', blurb: 'I am selling jewellery come buy some.', eventType: 2 }
+  const expected = {
+    type: 'RECEIVE_LOCAL_EVENT_BY_NAME',
+    lEvent
+  }
+
+  const actual = receiveLocalEventByName(lEvent)
+
+  expect(actual).toEqual(expected)
+})
