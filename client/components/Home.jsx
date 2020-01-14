7607e1790e0878a15fe98ccd3315fbe1e14700a1
Emily Coco Parkes <emilycoco@me.com>
2020-01-14 14:36:28 +1300

    simplify for new direction

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -1,17 +1,11 @@
 import React from 'react'
-import {connect} from 'react-redux'
-import {getApi} from '../actions/ticketmaster'
-
-import {Link} from 'react-router-dom'
+import Container from '@material-ui/core'
+import LabelBottomNavigation from './sections/BottomNav'
 
 class Home extends React.Component {
-  componentDidMount () {
-    this.props.dispatch(getApi())
-  }
-
   render () {
     return (
-      <div className='homepg'>
+      <Container>
         <div className='page-heading-section'>
           <div className='page-title-font'>
             <h1>Home</h1>
@@ -19,37 +13,14 @@ class Home extends React.Component {
           <h6>Do you want to eventify your life? To make it fuller and brighter? Find your ideal events here.</h6>
         </div>
 
-        <div className='page-section'>
-          <Link to={'/public-events'}>
-            <button className='pevent-buttons'>
-              Public Events
-            </button>
-          </Link>
-
-        </div>
-        <div className='page-section'>
-          <Link to={'/local-events'}>
-            <button className='levent-buttons'>
-              Local Events
-            </button>
-          </Link></div>
-
-        {this.props.ticketmaster.name}
-
         {/* <div className='page-section'>
           {this.state.errorMessage &&
             <h1>{this.state.errorMessage}</h1>
           }</div> */}
-
-      </div>
+        <LabelBottomNavigation/>
+      </Container>
     )
   }
 }
 
-const mapStateToProps = (state) => {
-  return {
-    ticketmaster: state.ticketmaster
-  }
-}
-
-export default connect(mapStateToProps)(Home)
+export default Home
