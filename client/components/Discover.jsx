c62b459a776119a6eef1026feee331b3939c6678
Emily Coco Parkes <emilycoco@me.com>
2020-01-20 15:10:20 +1300

    map full height w header

--- client/components/Discover.jsx
+++ client/components/Discover.jsx
@@ -42,7 +42,7 @@ TabPanel.propTypes = {
 
 function a11yProps (index) {
   return {
-    // id: `simple-tab-${index}`,
+    id: `simple-tab-${index}`,
     'aria-controls': `simple-tabpanel-${index}`
   }
 }
